8764e70be0e428a9712ef38a19494e7a3326b3dd
Vasanth Kumar G <vasanthgk02@gmail.com>
2023-02-26 23:43:10 +0530

    Time: 404 ms (13.38%), Space: 43.3 MB (22.97%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,20 +2,19 @@ class Solution {
     
     boolean helper(int arr[], int ind, int dp[]){
         if(ind == arr.length - 1) return true;
-        if(dp[ind] != -1) return dp[ind] == 1;
+        if(dp[ind] != 0) return dp[ind] == 2;
         for(int steps = 1; steps <= arr[ind]; steps++){
             if(helper(arr, ind + steps, dp) == true){
-                dp[ind + steps] = 1;
+                dp[ind + steps] = 2;
                 return true;
             }
         }
-        dp[ind] = 0;
+        dp[ind] = 1;
         return false;
     }
     
     public boolean canJump(int[] nums) {
         int dp[] = new int[nums.length];
-        Arrays.fill(dp, -1);
         return helper(nums, 0, dp);
     }
 }
